9e00dd8b40b9e2f2caf762dade6bbf66c4b3ef3e
Tommy Ju <tommyju25@gmail.com>
2025-09-09 17:13:13 -0700

    Polish up assets and starting lives

--- lives_display.py
+++ lives_display.py
@@ -3,13 +3,13 @@ from constants import screen_size
 from player import Player
 
 class LivesDisplay:
-    LIFE_IMAGE_OFFSET = 12
+    LIFE_IMAGE_OFFSET = 2
     LIFE_IMAGE_Y_POS = 16
 
     def __init__(self, player):
         self.player = player
-        self.life_image = pygame.image.load("./assets/graphics/pixel-heart.gif").convert_alpha()
-        self.life_image = pygame.transform.scale(self.life_image, (32, 32))
+        self.life_image = pygame.image.load("./assets/graphics/pixel_heart.png").convert_alpha()
+        self.life_image = pygame.transform.scale(self.life_image, (40, 40))
         self.life_x_start = screen_size.SCREEN_WIDTH - (self.life_image.get_width() + self.LIFE_IMAGE_OFFSET) * Player.STARTING_LIVES
 
     def draw(self, screen):
